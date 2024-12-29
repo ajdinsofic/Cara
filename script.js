86ae8f8d84c3905884da0231a1e25113e395c92e
ajdinsofic <ajdinsofa@gmail.com>
2024-12-29 19:18:07 +0100

    Half finishes design

---
+++ script.js
@@ -0,0 +1,15 @@
+let PromjenaBoje = () => {
+  var lista = document.querySelector("#sm-banner .banner-box span");
+  lista.style.color = "blue";
+};
+
+let VratiBoju = () => {
+  var lista = document.querySelector("#sm-banner .banner-box span");
+  lista.style.color = "white";
+};
+
+var Pocetni = () => {
+  var provjera = document.createElement("p");
+  provjera.style.color = "rgb(16, 116, 117)";
+  var listaPocetna = document.querySelectorAll("#navbar li a");
+};
